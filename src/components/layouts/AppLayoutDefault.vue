8b624dcfec724eefdb01dabf3515f8d0bcbf06d8
binhbdn <binh.buidn@gmail.com>
2021-09-13 17:19:36 +0700

    ADD: generic layout & layout default

---
+++ src/components/layouts/AppLayoutDefault.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <AppHeader />
+    <slot />
+    <AppFooter />
+  </div>
+</template>
+
+<script>
+import AppHeader from "@/components/sections/AppHeader";
+import AppFooter from "@/components/sections/AppFooter";
+
+export default {
+  name: "AppLayoutDefault",
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+};
+</script>
