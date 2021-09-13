b02e9bccdb1582dfed0473e229795d2bbbefbc11
binhbdn <binh.buidn@gmail.com>
2021-09-13 17:25:48 +0700

    ADD: global mixin & global component

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 
+import pageTitleMixin from "./mixins/pageTitleMixin";
+Vue.mixin(pageTitleMixin);
+
+import AppLayout from "./components/layouts/AppLayout.vue";
+Vue.component("AppLayout", AppLayout);
+
 Vue.config.productionTip = false;
 
 new Vue({
