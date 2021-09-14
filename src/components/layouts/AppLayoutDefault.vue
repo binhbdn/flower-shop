fa0624aa3c35350247c29d1d0ca1332027ac268f
binhbdn <binh.buidn@gmail.com>
2021-09-14 13:47:07 +0700

    RENAME

--- src/components/layouts/AppLayoutDefault.vue
+++ src/components/layouts/AppLayoutDefault.vue
@@ -1,20 +1,20 @@
 <template>
   <div>
-    <AppHeader />
+    <TheHeader />
     <slot />
-    <AppFooter />
+    <TheFooter />
   </div>
 </template>
 
 <script>
-import AppHeader from "@/components/sections/AppHeader";
-import AppFooter from "@/components/sections/AppFooter";
+import TheHeader from "@/components/sections/TheHeader";
+import TheFooter from "@/components/sections/TheFooter";
 
 export default {
   name: "AppLayoutDefault",
   components: {
-    AppHeader,
-    AppFooter,
+    TheHeader,
+    TheFooter,
   },
 };
 </script>
