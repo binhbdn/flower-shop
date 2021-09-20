063c99836c11b83e37f9cd0915f17445401488d2
binhbdn <binh.buidn@gmail.com>
2021-09-20 21:26:15 +0700

    ADD data & use v-for to render 3 carousel-item

--- src/components/sections/AppTestimonial.vue
+++ src/components/sections/AppTestimonial.vue
@@ -1,6 +1,10 @@
 <template>
-  <div id="myTestimonial" class="carousel slide" data-bs-ride="carousel">
-    <div class="carousel-indicators">
+  <div
+    id="myTestimonial"
+    class="testimonial carousel slide carousel-dark"
+    data-bs-ride="carousel"
+  >
+    <div class="carousel-indicators pb-4">
       <button
         v-for="index in 3"
         :key="index"
@@ -12,6 +16,31 @@
       ></button>
     </div>
     <div class="carousel-inner">
+      <div
+        v-for="item in testimonialItems"
+        :key="item.id"
+        :class="{ active: item.id == 1 }"
+        class="carousel-item"
+        data-bs-interval="5000"
+      >
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-12">
+              <div class="carousel-caption row justify-content-center">
+                <div class="col-lg-7">
+                  <div class="testimonial__text mx-5">
+                    <img
+                      src="data:image/webp;base64,UklGRmACAABXRUJQVlA4TFMCAAAvQgANEOdgkG2k7V7y/UWmYTZtGzVT/3+2U4nitm0j2fv/M2tPIYhkG/8SvPcT+JU1owkJXoD6m9l/rzXRFY0EKYAVEUqgSIK4IaBhQCsBU/kyYZNm3il9t+tCsm3bUdxcJIwxhUt0B+T4/f+Poo5kmElE/ydAyc8NYFlef0zNu+8LOJblx6npROn3RyO7KDy+ujpH5eGbzFS+rGaAtyR5P67cF2sQoJfU+X4+Aa7Pigjxdu/iCVyuiVe2DwAvayZZBBgekXwANmsnuRPon5FmYLMHZCtc7iENwKtggLlCtsJhVX2FBiDmHMll+XEZuRNi2cz9WL4/LKMJ6DKaE/fFpdQDXVF33u7Xj6UUYM4pnhkYUgowF8mFDGyW8oDL3c2PM8CY8oAVJbs+7sBlCb1hLrv7HegTCjDUSbIJ2Czh4KqTBTgsMcK7idQDMaEdfJ26E2KiAxppgssSE0wNNMKW0Aq+ke0wJDyEFgZ0idBOEeZHtINPzDC06iEkBDQJuQixlYPtnzM4n9n+hh5Cg+43/w2nGrnf7McFU41fLsrHJj8b5aeV2Re1kxq4jcrdqWwD2EM+ejXw3EN+Hk1lEXibmhbYAWevtjcHjGpcMMFueiBC0GOA1xMB/GMOVj1yQPfYAPMzgB6LEP8LWsE1e0N/8xCadbDrDX2zDdytg6NZD0ERDms0AkqeMLbaIMpOeFkTf0FMjYBvYi84TfLA8eFq7PcFBGUD8Pq1GvfnAHpJijTfu1y303xW0u+NJlOhTY3OXlmL81pzhtir0sdw1qzvaLoDAA=="
+                    />
+                    <p>“{{ item.content }}”</p>
+                    <h4>{{ item.name }}</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <button
       class="carousel-control-prev"
@@ -37,5 +66,29 @@
 <script>
 export default {
   name: "AppTestimonial",
+  data: function () {
+    return {
+      testimonialItems: [
+        {
+          id: 1,
+          name: "Trâm Anh",
+          content:
+            "Cảm ơn các bạn đã đem đến sự bất ngờ về món quà của người ấy cho mình. Mình thật sự hạnh phúc về đóa hoa ấy. Cảm ơn!",
+        },
+        {
+          id: 2,
+          name: "Alain Meilland",
+          content:
+            "Qua các bạn, tuy mình ở nước ngoài nhưng vẫn có thể gửi món quà yêu thương đến người thân ở Việt Nam, Cảm ơn nhiều.",
+        },
+        {
+          id: 3,
+          name: "Ngọc Thạch",
+          content:
+            "Mình rất dịch vụ của các bạn, rất nhanh chóng và đùng giờ, chúc các bạn thành công. Mình sẽ luôn ủng hộ các bạn.",
+        },
+      ],
+    };
+  },
 };
 </script>
