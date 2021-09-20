3228283e63449b2e65abb53dc42ecdb8157d5340
binhbdn <binh.buidn@gmail.com>
2021-09-20 11:55:19 +0700

    ADD: base elements for boostrap carousel

--- src/components/sections/AppTestimonial.vue
+++ src/components/sections/AppTestimonial.vue
@@ -1,5 +1,27 @@
 <template>
   <div id="myTestimonial" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+    </div>
+    <div class="carousel-inner">
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#myTestimonial"
+      data-bs-slide="prev"
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#myTestimonial"
+      data-bs-slide="next"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
   </div>
 </template>
 
