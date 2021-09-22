26f27e65558c561fb1095ca944f1e4f54dad4f02
binhbdn <binh.buidn@gmail.com>
2021-09-23 00:17:27 +0700

    just only ESLINT format

--- src/components/sections/AppTestimonial.vue
+++ src/components/sections/AppTestimonial.vue
@@ -75,7 +75,9 @@ export default {
   },
   created: function () {
     axios
-      .get("https://api.jsonstorage.net/v1/json/ba79d80e-fd4e-4084-a851-2eaa13e0e68a")
+      .get(
+        "https://api.jsonstorage.net/v1/json/ba79d80e-fd4e-4084-a851-2eaa13e0e68a"
+      )
       .then((response) => (this.testimonialItems = response.data))
       .catch((error) => console.log(error));
   },
