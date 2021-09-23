b3e4939a57d929a13c9f42e6396947b5b0388fa6
binhbdn <binh.buidn@gmail.com>
2021-09-23 23:08:17 +0700

    ADD data for TheNewArrivals

--- src/components/sections/TheNewArrivals.vue
+++ src/components/sections/TheNewArrivals.vue
@@ -8,5 +8,12 @@ import axios from "axios";
 
 export default {
   name: "TheNewArrivals",
+  data: function () {
+    return {
+      selectedFilterId: 0,
+      filterSet: null,
+      newArrivalsItems: null,
+    };
+  },
 };
 </script>
