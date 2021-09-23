daf5df0bec582c56c6fa06fe40a370a44b12643f
binhbdn <binh.buidn@gmail.com>
2021-09-23 23:09:25 +0700

    GET data from API for TheNewArrivals

--- src/components/sections/TheNewArrivals.vue
+++ src/components/sections/TheNewArrivals.vue
@@ -15,5 +15,17 @@ export default {
       newArrivalsItems: null,
     };
   },
+  created: function () {
+    // API endpoint name: new-arrivals-data
+    axios
+      .get(
+        `${process.env.VUE_APP_ROOT_API}/b9614b1d-567b-4e4c-9fe3-845b0a734ab5`
+      )
+      .then((response) => {
+        this.filterSet = response.data.filterSet;
+        this.newArrivalsItems = response.data.newArrivalsItems;
+      })
+      .catch((error) => console.log(error));
+  },
 };
 </script>
