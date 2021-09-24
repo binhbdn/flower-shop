dbc7ae06c47743dceebab54c19d670bdaf52716a
binhbdn <binh.buidn@gmail.com>
2021-09-24 23:17:32 +0700

    Insert AppBreadcrumb into About view

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="about">
+    <AppBreadcrumb>
+      Giới thiệu
+      <template #links><router-link to="/">Trang chủ</router-link></template>
+      <template #endpoint>Giới thiệu</template>
+    </AppBreadcrumb>
     <AppAboutUs />
     <AppTestimonial />
   </div>
 </template>
 
 <script>
+import AppBreadcrumb from "../components/sections/AppBreadcrumb.vue";
 import AppAboutUs from "../components/sections/AppAboutUs.vue";
 import AppTestimonial from "../components/sections/AppTestimonial.vue";
 
@@ -13,6 +19,7 @@ export default {
   name: "About",
   pageTitle: "Florist | Giới thiệu",
   components: {
+    AppBreadcrumb,
     AppAboutUs,
     AppTestimonial,
   },
