ef8808eb517907b408cd572f114ff357d18d5c3e
binhbdn <binh.buidn@gmail.com>
2021-09-27 17:55:32 +0700

    ADD Products view

---
+++ src/views/Products.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <AppBreadcrumb>
+      Cửa hàng
+      <template #links><router-link to="/">Trang chủ</router-link></template>
+      <template #endpoint>Sản phẩm</template>
+    </AppBreadcrumb>
+    <TheProducts />
+  </div>
+</template>
+
+<script>
+import AppBreadcrumb from "../components/sections/AppBreadcrumb.vue";
+import TheProducts from "../components/sections/TheProducts.vue";
+
+export default {
+  name: "Products",
+  pageTitle: "Florist | Sản phẩm",
+  components: {
+    AppBreadcrumb,
+    TheProducts,
+  },
+};
+</script>
