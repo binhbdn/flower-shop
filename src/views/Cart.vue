6e036fe5401cc7823a2e146e96110570156ca489
binhbdn <binh.buidn@gmail.com>
2021-09-28 20:40:46 +0700

    CREATE Cart view

---
+++ src/views/Cart.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <AppBreadcrumb>
+      Giỏ hàng
+      <template #links>
+        <router-link to="/">Trang chủ</router-link>
+        <router-link to="/san-pham">Sản phẩm</router-link>
+      </template>
+      <template #endpoint>Giỏ hàng</template>
+    </AppBreadcrumb>
+    <ShoppingCart />
+  </div>
+</template>
+
+<script>
+import AppBreadcrumb from "../components/sections/AppBreadcrumb.vue";
+import ShoppingCart from "../components/sections/TheShoppingCart.vue";
+
+export default {
+  name: "Cart",
+  pageTitle: "Florist | Liên hệ",
+  components: {
+    AppBreadcrumb,
+    ShoppingCart,
+  },
+};
+</script>
