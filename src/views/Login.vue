c339e5176cab929be77667412fd0699c62ce69f5
binhbdn <binh.buidn@gmail.com>
2021-09-29 20:58:43 +0700

    CREATE Register/Login views

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <AppBreadcrumb>
+      Tài khoản
+      <template #links><router-link to="/">Trang chủ</router-link></template>
+      <template #endpoint>Đăng nhập</template>
+    </AppBreadcrumb>
+    <TheLogin />
+  </div>
+</template>
+
+<script>
+import AppBreadcrumb from "../components/sections/AppBreadcrumb.vue";
+import TheLogin from "../components/sections/TheLogin.vue";
+
+export default {
+  name: "Login",
+  pageTitle: "Florist | Đăng nhập",
+  components: {
+    AppBreadcrumb,
+    TheLogin,
+  },
+};
+</script>
